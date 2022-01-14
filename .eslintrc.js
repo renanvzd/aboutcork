2b45a236b636581413d269fd959e8feceef92108
Renan Veronez Drechsler <renanvz@outlook.com>
2022-01-14 17:19:28 -0300

    Logo

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,5 +31,9 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'jsx-a11y/anchor-is-valid': 'off',
     'react/require-default-props': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'react/button-has-type': 'off'
   }
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
 import NavLink from './NavLink';
 import { Container } from './styles';
+import fourleaf from '../../assets/four-leaf.png';
+
 
 function Header() {
   return (
     <Container>
+      <div>
+        <img src={fourleaf} alt="Four Leaf" className='fourleaf' />
+        <h1>Cork</h1>
+      </div>
+
+
       <ul>
         <NavLink title="Home" path="/" />
         <NavLink title="Projects" path="/projects" includes />
